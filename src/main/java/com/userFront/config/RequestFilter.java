adc62ca089b76b6786cf27c61d48e28b91f613c8
ndonko <tndonko@gmail>
2018-03-30 11:26:31 +0100

    first commit

---
+++ src/main/java/com/userFront/config/RequestFilter.java
@@ -0,0 +1,65 @@
+package com.userFront.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.core.Ordered;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletResponse response = (HttpServletResponse) res;
+	    HttpServletRequest request = (HttpServletRequest) req;
+
+	        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+	        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+	        response.setHeader("Access-Control-Max-Age", "3600");
+	        response.setHeader("Access-Control-Allow-Credentials", "true");
+	        
+	        
+	        if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+	            try {
+	                chain.doFilter(req, res);
+	            } catch(Exception e) {
+	                e.printStackTrace();
+	            }
+	        } else {
+	            System.out.println("Pre-flight");
+	            response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE");
+	            response.setHeader("Access-Control-Max-Age", "3600");
+	            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type," +
+	                    "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with");
+	            response.setStatus(HttpServletResponse.SC_OK);
+	        }
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/userFront/domain/security/Authority.java
@@ -0,0 +1,25 @@
+package com.userFront.domain.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Created by z00382545 on 10/20/16.
+ */
+public class Authority implements GrantedAuthority{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final String authority;
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+	
+}
